058120e225686eb1e663f5071f889dae35098380
elifdinarli <100302249+elifdinarli@users.noreply.github.com>
2024-07-02 22:03:05 +0200

    Update form.py

--- form.py
+++ form.py
@@ -35,9 +35,9 @@ with st.form(key="language selection"):
         action = f"Button clicked: See your options ({language})"
         log_action_to_gsheet(language, action)
         if language == "Spanish":
-            st.markdown(f"[Babbel is the right place to learn {language}!](https://my.babbel.com/en_GB/product-preview/SPA/default/node/0)")
+            st.link_button(f"[Babbel is the right place to learn {language}!](https://my.babbel.com/en_GB/product-preview/SPA/default/node/0)")
         else:
-            st.markdown(f"[Let Toucan help you while we're adding {language}!](https://jointoucan.com/)")
+            st.link_button(f"[Let Toucan help you while we're adding {language}!](https://jointoucan.com/)")
 
 
 
