48a09db1f702ec8cb818fba39c1b94b0fcd18aad
elifdinarli <100302249+elifdinarli@users.noreply.github.com>
2024-07-02 21:39:27 +0200

    Update form.py

--- form.py
+++ form.py
@@ -24,7 +24,7 @@ def log_action_to_gsheet(language, action):
     sheet.append_row(row)
 
 
-with st.form(key="language selection")
+with st.form(key="language selection"):
     language = st.selectbox("Which language do you want to learn?", ["Spanish", "Japanese"])
     submit_button = st.form_submit_button(label='Get Started!')
     if submit_button:
