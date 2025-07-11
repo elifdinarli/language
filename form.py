2c0db475a28db381938ca61d3518f4239897115a
elifdinarli <100302249+elifdinarli@users.noreply.github.com>
2025-07-11 22:40:34 +0200

    Update form.py

--- form.py
+++ form.py
@@ -7,42 +7,91 @@
 import streamlit as st
 import requests as r
 import pandas as pd
-import gspread
-from google.oauth2.service_account import Credentials
-import json
-from datetime import datetime
+
+
+# In[ ]:
+
+
+st.title("Contentful Bulk Edit Tool - MVP")
+st.subheader("Example use case: creating banner sections with an existing Flags CTA")
+
+
+# In[ ]:
+
+
+headers = {'Authorization': st.secrets["token"], 
+           'Content-Type': 'application/vnd.contentful.management.v1+json'}
+
+
+# In[ ]:
+
+
+file = st.file_uploader("Upload a CSV")
+
+
+if file is not None:
+    df = pd.read_csv(file)
+    st.write(df)
+else:
+    st.write("Please upload a file.")
 
 
 
-def log_action_to_gsheet(language, action):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
-    client = gspread.authorize(creds)
-
-    sheet_id = "1ltnW3wecxJwgjMioigtBR5jz_o2GAPmnHJ1rHpcPu7Q"
-    sheet = client.open_by_key(sheet_id).sheet1
-
-    # Add a new row with the logged action
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    row = [language, action, timestamp]
-    sheet.append_row(row)
+# In[43]:
 
 
-with st.form(key="language selection"):
-    language = st.selectbox("Which language do you want to learn?", ["Spanish", "Japanese"], index=None, placeholder="Select")
-    submit_button = st.form_submit_button(label='Get Started!')
-    if submit_button:
-        action = f"Button clicked: See your options ({language})"
-        log_action_to_gsheet(language, action)
-        if language == "Spanish":
-            st.link_button(f"Babbel is the right place to learn {language}!", "https://my.babbel.com/en_GB/product-preview/SPA/default/node/0")
-        else:
-            st.link_button(f"Let Toucan help you while we're adding {language}!", "https://jointoucan.com/")
+def banner(content_type, entry_title, color_palette, title, layout, cta):
+    banner = {
+        'fields': {
+            'entryTitle': {'en-US': entry_title},
+            'colorPalette': {'en-US': color_palette},
+            'titleRichText': {'en-US': {
+                'data': {},
+                'content': [{
+                    'data': {},
+                    'content': [{
+                        'data': {},
+                        'marks': [],
+                        'value': title,
+                        'nodeType': 'text'
+                    }],
+                    'nodeType': 'paragraph'
+                }],
+                'nodeType': 'document'
+            }},
+            'layout': {'en-US': layout},
+            'callToAction': {'en-US': [
+                {
+                    'sys': {
+                        'type': 'Link',
+                        'linkType': 'Entry',
+                        'id': cta
+                    }
+                }
+            ]}
+        }
+    }
+    headers = {'Authorization': st.secrets["token"], 
+               'Content-Type': 'application/vnd.contentful.management.v1+json',
+               'X-Contentful-Content-Type': content_type}
+    create = r.post("https://api.contentful.com/spaces/zuzqvf4m2o58/environments/master/entries", headers=headers, json=banner)
+    result = create.json()
+    return(result['sys']['id'])
 
 
+# In[32]:
 
 
+for index, row in df.iterrows():
+    entry_title = row['entry_title']
+    color_palette = row['color_palette']
+    title = row['title']
+    layout = row['layout']
+    cta = row['cta']
+    entry_id = banner(entry_title, color_palette, title, layout, cta)
+    url = "https://app.contentful.com/spaces/zuzqvf4m2o58/environments/master/entries/"+str(entry_id)
+    st.write(entry_title)
+    st.link_button("Click me", url)
 
 
-
-
+# In[ ]:
